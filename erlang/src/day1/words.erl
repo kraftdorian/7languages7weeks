7aa9d081ef1319392a1a55907cacd4aa7d5b68ae
kraftdorian <work.kajzer.damian@gmail.com>
2021-04-17 15:28:58 +0200

    Feature:- Added split method but it needs further implementation.

--- erlang/src/day1/words.erl
+++ erlang/src/day1/words.erl
@@ -1,9 +1,18 @@
 -module(words).
--export([main/0]).
+-export([split/4, main/0]).
 
 count([], Count) -> Count;
 count([_|Words], Count) -> count(Words, Count + 1).
 count(String) -> count(string:lexemes(String, " "), 0).
+
+% TODO: Figure out how to actually reverse the result.
+% words:split("Erlang is fun!", 32, [], []).
+split([], _, Words, Acc) -> lists:merge(Words, [Acc]);
+split([Head|Tail], Separator, Words, Acc) ->
+  case Head of
+    Separator -> split(Tail, Separator, [Acc|Words], []);
+    _ -> split(Tail, Separator, Words, [Head|Acc])
+  end.
 
 main() ->
   String = "Erlang is fun!",
