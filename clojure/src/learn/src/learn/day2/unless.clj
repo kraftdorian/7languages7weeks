9b26bf765978d9e0750854c5ba680704fc0a0507
kraftdorian <75522596+kraftdorian@users.noreply.github.com>
2021-05-02 15:49:32 +0200

    Refactor:- Adjusted formatting.

--- clojure/src/learn/src/learn/day2/unless.clj
+++ clojure/src/learn/src/learn/day2/unless.clj
@@ -3,8 +3,6 @@
 (defmacro unless [condition condition_false condition_true]
   (list 'if (list 'not condition) condition_false condition_true))
 
-(defn -main
-  []
+(defn -main []
   (unless (> 1 2) (println "1 < 2") (println "1 > 2"))
-  (unless (< 1 2) (println "1 > 2") (println "1 < 2"))
-)
+  (unless (< 1 2) (println "1 > 2") (println "1 < 2")))
