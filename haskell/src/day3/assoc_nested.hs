ad0794b605822b73211d829af8ca6041296d2522
kraftdorian <work.kajzer.damian@gmail.com>
2021-06-27 15:21:11 +0200

    Chore:- Added link to solution in another language.

--- haskell/src/day3/assoc_nested.hs
+++ haskell/src/day3/assoc_nested.hs
@@ -3,6 +3,9 @@ module AssocNested where
   -- Tried flattening the list before processing and other stuff but no idea.
   -- So, for now I just changed the behavior from ./assoc.hs to search for keys.
 
+  -- I've managed to complete the algorithm in JavaScript:
+  -- https://gist.github.com/kraftdorian/61022b5a2568b87d1873899c548cfd08
+
   -- (findAssocKey "bar" [("foo","bar"), ("bar", "baz")]) >>= return
   findAssocKey :: (Eq t0, Eq t1) => t0 -> [(t0, t1)] -> Maybe (t0, t1)
   findAssocKey _ [] = Nothing
