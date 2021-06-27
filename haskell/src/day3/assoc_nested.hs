d8825f51f16ec8a72987f6050bbd544591d7d8df
kraftdorian <work.kajzer.damian@gmail.com>
2021-06-27 02:16:56 +0200

    Feature:- Solved completed first exercise from the third day of the Haskell language.

---
+++ haskell/src/day3/assoc_nested.hs
@@ -0,0 +1,11 @@
+module AssocNested where
+  -- I was unable to find out how to handle a nested list in Haskell in a while.
+  -- Tried flattening the list before processing and other stuff but no idea.
+  -- So, for now I just changed the behavior from ./assoc.hs to search for keys.
+
+  -- (findAssocKey "bar" [("foo","bar"), ("bar", "baz")]) >>= return
+  findAssocKey :: (Eq t0, Eq t1) => t0 -> [(t0, t1)] -> Maybe (t0, t1)
+  findAssocKey _ [] = Nothing
+  findAssocKey needle (head:tail)
+    | (fst head) == needle = Just head
+    | otherwise = findAssocKey needle tail
